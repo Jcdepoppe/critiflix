d5a3ce5a1b9bb159dd2791befbd1020cc5e6ed69
jcdepoppe <jcdepoppe@comcast.net>
2018-11-16 16:54:55 -0800

    added basic user model

---
+++ src/main/java/com/critiflix/app/models/User.java
@@ -0,0 +1,47 @@
+package com.critiflix.app.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @Size(min=1, message="You must enter your name")
+    private String alias;
+    @Size(min=1, message="You must enter an email")
+    @Email(message="Email must be valid")
+    private String email;
+    @Size()
+    private int zipcode;
+    @Size(min=8, message="Password must be at least 8 characters")
+    private String password;
+    @Transient
+    private String passwordConfirmation;
+    @Column(updatable=false)
+    private Date createdAt;
+    private Date updatedAt;
+    
+    
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+}
