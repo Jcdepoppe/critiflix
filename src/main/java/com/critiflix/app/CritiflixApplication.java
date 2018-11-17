ad194e600edc6ffc530f6f0f978693b371f91161
jcdepoppe <jcdepoppe@comcast.net>
2018-11-16 16:39:15 -0800

    first commit

---
+++ src/main/java/com/critiflix/app/CritiflixApplication.java
@@ -0,0 +1,12 @@
+package com.critiflix.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CritiflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CritiflixApplication.class, args);
+	}
+}
