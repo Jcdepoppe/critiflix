83bc0dab745035c82aae39152681d845ac3a1ede
jcdepoppe <jcdepoppe@comcast.net>
2018-11-20 11:16:26 -0800

    added update and delete rating functionality

--- src/main/java/com/critiflix/app/controllers/RatingController.java
+++ src/main/java/com/critiflix/app/controllers/RatingController.java
@@ -1,12 +1,17 @@
 package com.critiflix.app.controllers;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.critiflix.app.models.Rating;
+import com.critiflix.app.models.User;
 import com.critiflix.app.services.RatingService;
 
 @Controller
@@ -19,19 +24,43 @@ public class RatingController {
 	}
 	
 	@RequestMapping("")
-	public String index(Model model, @ModelAttribute("rating") Rating rating) {
+	public String ratingIndex(Model model, @ModelAttribute("rating") Rating rating) {
 		return "newReview.jsp";
 	}
 	
 	@RequestMapping("/create")
-	public String create(@ModelAttribute("rating") Rating rating, BindingResult result) {
+	public String createRating(@ModelAttribute("rating") Rating rating, BindingResult result, HttpSession session) {
 		double sum = rating.getCleanliness() + rating.getFoodQuality() + rating.getService();
-		System.out.println(sum);
 		double avg = sum / 3;
-		System.out.println(avg);
 		rating.setAggregate(avg);
+		User user = (User)session.getAttribute("user");
+		rating.setUser(user);
+		//Get the theater object and add the rating to its rating list
 		rating = ratingService.create(rating);
 		return "redirect:/theaters";
 	}
 	
+	@RequestMapping("/{id}/edit")
+	public String editRating(Model model, @PathVariable("id") Long id) {
+		Rating rating = ratingService.getRatingById(id);
+		model.addAttribute("rating", rating);
+		return "editRating.jsp";
+	}
+	
+	@RequestMapping(value="/{id}/edit", method=RequestMethod.PUT)
+	public String updateRating(@ModelAttribute("rating") Rating rating, HttpSession session) {
+		
+		double sum = rating.getCleanliness() + rating.getFoodQuality() + rating.getService();
+		rating.setAggregate(sum / 3);
+		User user = (User)session.getAttribute("user");
+		rating.setUser(user);
+		ratingService.updateRating(rating);
+		return "redirect:/theaters";
+	}
+	
+	@RequestMapping(value="/{id}/edit", method=RequestMethod.DELETE)
+	public String destroyRating(@PathVariable("id") Long id) {
+		ratingService.deleteRating(id);
+		return "redirect:/theaters";
+	}
 }
---
+++ src/main/webapp/WEB-INF/editRating.jsp
@@ -0,0 +1,61 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>  
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Edit your review</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+    crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+    crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="card">
+		<div class="card-body">
+			<h2>THEATER NAME</h2>
+			<p>Location info</p>
+			<p>Rate the movie theater on these three catagories:</p>
+			<form:form method="POST" action="/ratings/${rating.id}/edit"
+				modelAttribute="rating">
+				<input type="hidden" name="_method" value="put">
+				<form:label path="cleanliness">Cleanliness:</form:label>
+				<form:select path="cleanliness">
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+				</form:select>
+				<form:label path="foodQuality">Food Quality:</form:label>
+				<form:select path="foodQuality">
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+				</form:select>
+				<form:label path="service">Service:</form:label>
+				<form:select path="service">
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+				</form:select>
+				<p>
+					<form:label path="description">Write a review:</form:label>
+					<form:textarea path="description" />
+					<input type="submit" value="Submit" />
+				</p>
+			</form:form>
+			<form:form method="POST" action="/ratings/${rating.id}/edit">
+				<input type="hidden" name="_method" value="delete">
+				<input type="submit" value="Delete" />
+			</form:form>
+		</div>
+	</div>
+</body>
+</html>
