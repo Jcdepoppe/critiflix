0e836a9ec9fe57c6b5577a890549af05cac266db
jcdepoppe <jcdepoppe@comcast.net>
2018-11-19 19:56:28 -0800

    added theater rating function

--- src/main/java/com/critiflix/app/controllers/RatingController.java
+++ src/main/java/com/critiflix/app/controllers/RatingController.java
@@ -1,5 +1,37 @@
 package com.critiflix.app.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.critiflix.app.models.Rating;
+import com.critiflix.app.services.RatingService;
+
+@Controller
+@RequestMapping("/ratings")
 public class RatingController {
+	private final RatingService ratingService;
+
+	public RatingController(RatingService ratingService) {
+		this.ratingService = ratingService;
+	}
+	
+	@RequestMapping("")
+	public String index(Model model, @ModelAttribute("rating") Rating rating) {
+		return "newReview.jsp";
+	}
+	
+	@RequestMapping("/create")
+	public String create(@ModelAttribute("rating") Rating rating, BindingResult result) {
+		double sum = rating.getCleanliness() + rating.getFoodQuality() + rating.getService();
+		System.out.println(sum);
+		double avg = sum / 3;
+		System.out.println(avg);
+		rating.setAggregate(avg);
+		rating = ratingService.create(rating);
+		return "redirect:/theaters";
+	}
 	
 }
